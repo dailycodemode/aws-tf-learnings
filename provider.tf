1bbc051da31a0643daccf8a39b430a81e9c05659
dailycodemode <steedmanman@gmail.com>
2025-12-17 21:14:37 +0000

    fight

--- provider.tf
+++ provider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-steed"
+    key            = "dev/terraform.tfstate" # Path inside the bucket
+    region         = "eu-west-1"
+  }
+}
